5489441f8cde588ea0ab33bdd7dce48edbf38304
Tanushree <mohantytanushree234@gmail.com>
2023-10-02 21:37:26 +0530

    status:styles updated

--- Front-end/src/dummyData/storiseData.js
+++ Front-end/src/dummyData/storiseData.js
@@ -3,43 +3,59 @@ const dummyIMG = 'https://images.unsplash.com/photo-1687360440747-cafed773d446?i
 const users = [
   {
     name: 'John Doe',
-    profileImg: dummyIMG,
+    profileImg: 'https://i.pinimg.com/564x/43/cd/7c/43cd7c65d590d2f41c05a23f3dfe82d4.jpg',
   },
   {
     name: 'Jane Smith',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/564x/92/6e/c1/926ec1304586f8a447dde710a4f7d7ff.jpg",
   },
   {
     name: 'Michael Johnson',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/564x/0e/92/4b/0e924be21a34aae92f299234d1c27ad5.jpg",
   },
   {
     name: 'Emily Davis',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/22/b8/c2/22b8c28f863f529e2f8af71186bbe5d4.jpg",
   },
   {
     name: 'David Wilson',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/ad/2c/ec/ad2cec757cc4dcf250a79a3e8a6e0ab5.jpg",
   },
   {
     name: 'Sarah Brown',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/dd/5c/fb/dd5cfb6014dcab0a03405f802e9fe27a.jpg",
   },
   {
     name: 'Daniel Lee',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/28/14/d3/2814d3f4b243a87d2150363729f3d732.jpg",
   },
   {
     name: 'Jennifer Clark',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/6c/11/92/6c1192eacbb13d1e39336d1026c7f1ae.jpg",
   },
   {
     name: 'Matthew White',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/35/55/c5/3555c5ddf44b04a74643bd67a9b9fc0b.jpg",
   },
   {
     name: 'Olivia Martin',
-    profileImg: dummyIMG,
+    profileImg: "https://i.pinimg.com/236x/dd/5c/fb/dd5cfb6014dcab0a03405f802e9fe27a.jpg",
+  },
+  {
+    name: 'John Doe',
+    profileImg: 'https://i.pinimg.com/564x/43/cd/7c/43cd7c65d590d2f41c05a23f3dfe82d4.jpg',
+  },
+  {
+    name: 'Jane Smith',
+    profileImg: "https://i.pinimg.com/564x/92/6e/c1/926ec1304586f8a447dde710a4f7d7ff.jpg",
+  },
+  {
+    name: 'Michael Johnson',
+    profileImg: "https://i.pinimg.com/564x/0e/92/4b/0e924be21a34aae92f299234d1c27ad5.jpg",
+  },
+  {
+    name: 'Emily Davis',
+    profileImg: "https://i.pinimg.com/236x/22/b8/c2/22b8c28f863f529e2f8af71186bbe5d4.jpg",
   },
 ];
 
